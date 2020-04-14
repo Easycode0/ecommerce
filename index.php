2a214697f92502a4119e9da3860569b9179a30e3
Anderson Alves <Anderson2018_ti@hotmail.com>
2020-04-13 23:26:13 -0300

    Criando template admin

--- index.php
+++ index.php
@@ -4,22 +4,34 @@
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 
 
 $app = new Slim();
 
-$app->config('debug', true);//ta debug irar dizer os erros
+$app->config('debug', true);
+
+//ta debug irar dizer os erros
 
 $app->get('/', function() {
     
 
 	$page = new Page();///Chamando o construct que vai add o heade na tela
+	$page->setTpl("index");
+	
+
+});
+$app->get('/admin', function() {
+    
+
+	$page = new PageAdmin();///Chamando o construct que vai add o heade na tela
 
 	$page->setTpl("index");
 	
 
 });
+
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -18,15 +18,14 @@ class Page{
 
     ];
 
-	public function __construct($opts = array()){
+	public function __construct($opts = array(), $tpl_dir = "/views/"){
         //Função array_marge é usada para sobrescrever array
         $this->options = array_merge($this->defaults, $opts);
 
 	   $config = array(
 
-				"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/ecommerce/views/",
-				"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/ecommerce/views-cache/",
-                "auto_escape"   => false,
+				"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
+				"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 				"debug"         => false // set to false to improve the speed
 			 );
 
